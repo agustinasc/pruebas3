6cf353bd135f9a179c11a31ffe39f9fbeda72921
agustinasc <agustina.santillancastro@gmail.com>
2021-02-26 15:55:55 -0300

    UPDATE/Arreglando background con img locales

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ const img4 = document.getElementById('img4');
 ////////////////////      EVENTOS ///////////////
 
 img1.addEventListener('click', (e)=>{
-    //backCover.classList.add('addImg');
-    backCover.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgoeoHRiZHCYepPof98Ch9yS-NC6xNBLhAZg&usqp=CAU)"
+    backCover.style.background = "url(./img/coverHarri1.jpg) no-repeat center/cover";
+    // backCover.style.backgroundSize = "cover";
     title.innerHTML = "Harry Potter";
 })
 
-img2.addEventListener('click', ()=>{    
-    backCover.style.backgroundImage = "url(https://gamerstyle.com.mx/wp-content/uploads/2020/06/xHogwarts-Dark-Legacy-rumores-juego-harry-potter-gamer-style.jpg.pagespeed.ic.B1NRhLc6dF.webp)";
+img2.addEventListener('click', (e)=>{    
+    backCover.style.background = "url(./img/coverHarry2.jpg) no-repeat center/cover";
     title.innerHTML = "Harry Potter 2"
 })
-img3.addEventListener('click', ()=>{
-    backCover.style.backgroundImage = "url(https://www.lavanguardia.com/files/image_449_220/uploads/2020/12/09/5fd1029065a27.jpeg)";
+img3.addEventListener('click', (e)=>{
+    backCover.style.background = "url(./img/coverFriends.jpg) no-repeat center/cover";
     title.innerHTML = "Friends"
 })
-img4.addEventListener('click', ()=>{
-    backCover.style.backgroundImage = "url(https://www.laguiago.com/wp-content/uploads/2021/02/Tableros-gambito-de-dama.jpg";
+img4.addEventListener('click', (e)=>{
+    backCover.style.background = "url(./img/coverQueens.jpg) no-repeat center/cover";
     title.innerHTML = "Queen's Gambit"
 })
-img5.addEventListener('click', ()=>{
-    backCover.style.backgroundImage = "url(https://www.liderempresarial.com/wp-content/uploads/2019/04/GOT-750x422.jpg)";
+img5.addEventListener('click', (e)=>{
+    backCover.style.background = "url(./img/coverGot.jpg) no-repeat center/cover";
     title.innerHTML = "Game of Thrones";
 })
-img6.addEventListener('click', ()=>{
-    backCover.style.backgroundImage = "url(https://www.republica.com/wp-content/uploads/2020/01/modern-family.jpg)";
+img6.addEventListener('click', (e)=>{
+    backCover.style.background = "url(./img/modern.jpg) no-repeat center/cover";
     title.innerHTML = "Modern Family";
 })
 
